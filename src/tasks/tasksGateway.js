dd991f5161d6b41dbdbd8d4a94fef222a83ba931
eposha <freedoctime@gmail.com>
2020-03-12 12:33:41 +0300

    first commit

---
+++ src/tasks/tasksGateway.js
@@ -0,0 +1,50 @@
+const baseUrl =
+    "https://5e39354daad22200149624f6.mockapi.io/api/v1/tasks";
+
+export const createTask = taskData => {
+    return fetch(baseUrl, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(taskData)
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error("Failed to create task");
+        }
+    });
+}
+
+export const fetchTasksList = () => {
+    return fetch(baseUrl)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+};
+
+
+export const updateTask = (taskId, taskData) => {
+    return fetch(`${baseUrl}/${taskId}`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(taskData)
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error("Failed to create task");
+        }
+    });
+}
+
+export const deleteTask = (taskId) => {
+    return fetch(`${baseUrl}/${taskId}`, {
+        method: "DELETE"
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error("Failed to delete task");
+        }
+    });
+}
